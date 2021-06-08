78a90978ea1423ca286060914b500349db0404aa
sajan.arora <sajan.arora@grazitti.com>
2021-06-09 01:13:09 +0530

    BST 1

--- codes/BinarySearchTree/bst.java
+++ codes/BinarySearchTree/bst.java
@@ -132,6 +132,246 @@ public class bst {
         return node;
     }
 
+
+    // replace with sum of larger values
+    static int sum = 0;
+    public static void rwsol(Node node){
+        if(node == null) return;
+        
+        rwsol(node.right);
+        // work in InArea
+        int data = node.data;  
+        node.data = sum;
+        sum += data;
+        rwsol(node.left);
+    }
+
+
+    // abandoned code 
+    // public static Node findForLCA(Node node, int data){
+        //     // write your code here
+        //         if(node == null) return null;
+        //         if(node.data == data){
+    //             return node;
+    
+    //         }else if(data < node.data){
+    //             // go to left
+    //             Node lres = find(node.left,data);
+    //             if(lres.data == data){
+        //                 return node;
+    //             }
+    //         }else{
+        //             // data > node.data go to right
+        //             Node rres = find(node.right,data);
+        //             if(rres.data == data){
+            //                 return node;
+    //             }
+    
+    //         }
+    //         return null;
+    // }
+    
+    // LCA
+    // Node to root path for lca Very bad for LCA dont use this
+    public static ArrayList<Integer> n2rp(Node node,int data){
+        if(node == null) return new ArrayList<Integer>();
+        if(node.data == data){
+            ArrayList<Integer> base = new ArrayList<>();
+            base.add(node.data);
+            return base;
+        }else if(data < node.data){
+            ArrayList<Integer> llres = n2rp(node.left,data);
+            if(llres.size() > 0){
+                llres.add(node.data);
+                return llres;
+            }
+        }else{
+            ArrayList<Integer> rres = n2rp(node.right,data);
+            if(rres.size() > 0){
+                rres.add(node.data);
+                return rres;
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    public static int lca(Node node, int d1, int d2) {
+        if(d1 > node.data && d2 > node.data){
+            // on the right side;
+            return lca(node.right, d1, d2);
+        }else if(d1 < node.data && d2 < node.data){
+            // on the left side;
+            return lca(node.left,d1,d2);
+        }else{
+            // this is it d1 in left and d2 in right
+            return node.data;
+        }
+    }
+
+    // Very bad approach for BST o(n) never do this
+    // public static int lca2(Node node, int d1, int d2) {
+    
+    //     ArrayList<Integer> l1 = n2rp(node,d1);
+    //     ArrayList<Integer> l2 = n2rp(node,d2);
+
+    //     int res = -1;
+    //     int i = l1.size()-1;
+    //     int j = l2.size()-1;
+    //     while(i>=0 && j>=0 && l1.get(i) == l2.get(j)){
+    //         res = l1.get(i);
+    //         i--;
+    //         j--;
+    //     }
+    //     return res;
+    // }
+
+    // print in range
+    public static void pir(Node node, int d1, int d2) {
+        if(node == null) return;
+        
+        if(d1 < node.data && d2 < node.data){
+            // left side
+            pir(node.left,d1,d2);
+        }else if(d1 > node.data && d2 > node.data){
+            // right side
+            pir(node.right,d1,d2);
+        }else{
+            // answer
+            // as we have to print inInorder we go to left first then print and then right side
+            pir(node.left,d1,d2);
+            System.out.println(node.data);
+            pir(node.right,d1,d2);
+        }
+    }
+
+    // Target Sum Pair in BST
+    // method 1 0(nh)+0(h) h-height
+    public static void printTargetSumPair1(Node node,Node root,int  target) {
+        if(node == null) return;
+        int n1 = node.data;
+        int n2 = target-n1;
+        printTargetSumPair1(node.left, root, target);
+        //  in ORDER
+        if(n1 < n2 && find(root,n2) == true){
+            System.out.println(n1+" "+n2);
+        }
+        printTargetSumPair1(node.right, root, target);
+    }
+    // method 2 0(n)+0(n)
+    public static void inOrderFiller(Node node,ArrayList<Integer> list){
+        if(node == null) return;
+        inOrderFiller(node.left, list);
+        list.add(node.data);
+        inOrderFiller(node.right, list);
+    }
+    public static void printTargetSumPair2(Node node,int  target) {
+        ArrayList<Integer> inorder = new ArrayList<>();
+        inOrderFiller(node, inorder);
+        int i = 0;
+        int j = inorder.size()-1;
+        while(i<j){
+            int n1 = inorder.get(i);
+            int n2 = inorder.get(j);
+            if(n1 + n2 > target){
+                j--;
+            }else if(n1+n2 < target){
+                i++;
+            }else{
+                System.out.println(n1+" "+n2);
+                i++;
+                j--;
+            }
+        }
+    } 
+
+    
+    // method 3 0(n)+0(h) h-height
+    // Two stack iterative Approach because we can stop recurstion
+    public static class Pair {
+        Node node;
+        int state;
+    
+        Pair(Node node, int state) {
+          this.node = node;
+          this.state = state;
+        }
+      }
+
+      public static int inorderItr(Stack<Pair> st) {
+        while(st.size() > 0) {
+            Pair p = st.peek();
+
+            if(p.state == 0) {
+                // left child
+                if(p.node.left != null) {
+                    st.push(new Pair(p.node.left, 0));
+                }
+                p.state++;
+            } else if(p.state == 1) {
+                // right child
+                if(p.node.right != null) {
+                    st.push(new Pair(p.node.right, 0));
+                }
+                p.state++;
+                return p.node.data;
+            } else {
+                // pop
+                st.pop();
+            }
+        }
+        return -1;
+    }
+
+    public static int revInorderItr(Stack<Pair> st) {
+        while(st.size() > 0) {
+            Pair p = st.peek();
+
+            if(p.state == 0) {
+                // right child
+                if(p.node.right != null) {
+                    st.push(new Pair(p.node.right, 0));
+                }
+                p.state++;
+            } else if(p.state == 1) {
+                // left child
+                if(p.node.left != null) {
+                    st.push(new Pair(p.node.left, 0));
+                }
+                p.state++;
+                return p.node.data;
+            } else {
+                // pop
+                st.pop();
+            }
+        }
+        return -1;
+    }
+
+
+    public static void printTargetSumPair3(Node node, int target) {
+        Stack<Pair> ls = new Stack<>();
+        Stack<Pair> rs = new Stack<>();
+
+        ls.push(new Pair(node, 0));
+        rs.push(new Pair(node, 0));
+
+
+        int left = inorderItr(ls);
+        int right = revInorderItr(rs);
+
+        while(left < right) {
+            int sum = left + right;
+            if(sum > target) {
+                right = revInorderItr(rs);
+            } else if(sum < target) {
+                left = inorderItr(ls);
+            } else {
+                System.out.println(left + " " + right);
+                left = inorderItr(ls);
+                right = revInorderItr(rs);
+            }
+        }
+    } 
     public static void fun () {
         int[] data= {10,20,30,40,50,60,70,80,90};
         Node root = construct(data,0,data.length-1);
