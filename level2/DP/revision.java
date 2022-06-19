1a0706b157c2751f9d8a65ee518bc08b4cc9f385
SajanArora <sajanarora@zomentum.com>
2022-06-19 22:01:49 +0530

    update

--- level2/DP/revision.java
+++ level2/DP/revision.java
@@ -292,6 +292,216 @@ public class revision {
         return length;
     }
 
+    // Maximum Length Of Pair Chain
+    public static int findLongestChain(int[][] pairs) {
+        Arrays.sort(pairs , (a,b)->{
+            return a[1] - b[1];
+        });
+        // LIS
+        int[] dp = new int[pairs.length];
+        dp[0] = 1;
+        int length = dp[0];
+        for(int i=1;i<pairs.length;i++){
+            int max = 0;
+            for(int j=i-1;j>=0;j--){
+                if(pairs[j][1] < pairs[i][0]) {
+                    max = Math.max(max,dp[j]);
+                }
+            }
+            dp[i] = max+1;
+            length = Math.max(length,dp[i]);
+        }
+        return length;
+    }
+
+    // Box Stacking
+    // https://practice.geeksforgeeks.org/problems/box-stacking/1/
+    public static class Box implements Comparable<Box> {
+        int l;
+        int b;
+        int h;
+        int area; // in terms of base;
+        public Box(int l,int b,int h){
+            this.l = l;
+            this.b = b;
+            this.h = h;
+            this.area = this.l * this.b;
+        }
+        public int compareTo(Box o) {
+            if(o.area != this.area)
+                return o.area - this.area; // decreasing order  
+            else
+                return o.h - this.h; // if area not equal do it with height.... (decreasing order)
+
+            // return o.l*o.b*o.h - this.l*this.b*this.h; (using volume)
+        }
+    }
+    public static int maxHeight(int[] height, int[] width, int[] length, int n){
+        Box[] arr = new Box[3 * n];
+        int j = 0;
+        for(int i=0;i<n;i++){
+            // LBH
+            if(length[i] > width[i]){
+                arr[j++] = new Box(length[i],width[i],height[i]);
+            }else{
+                arr[j++] = new Box(width[i],length[i],height[i]);
+            }
+            // HBL
+            if(height[i] > width[i]){
+                arr[j++] = new Box(height[i],width[i],length[i]);
+            }else{
+                arr[j++] = new Box(width[i],height[i],length[i]);
+            }
+            // LHB
+            if(length[i] > height[i]){
+                arr[j++] = new Box(length[i],height[i],width[i]);
+            }else{
+                arr[j++] = new Box(height[i],length[i],width[i]);
+            }
+        }
+        Arrays.sort(arr);
+        int[] dp = new int[3 * n];
+        dp[0] = arr[0].h;
+        int res = dp[0];
+        for(int i=1;i<3*n;i++){
+            int max = 0;
+            for(int k=i-1;k>=0;k--){
+                if(arr[k].l > arr[i].l && arr[k].b > arr[i].b){
+                    max = Math.max(max,dp[k]);
+                }
+            }
+            dp[i] = max + arr[i].h;
+            res = Math.max(res,dp[i]);
+        }
+        return res;
+    }
+    // https://leetcode.com/problems/maximum-height-by-stacking-cuboids/
+    public int maxHeight2(int[][] cuboids) {
+        Box[] arr = new Box[cuboids.length];
+        int j = 0;
+        for(int[] box : cuboids) {
+            Arrays.sort(box);
+            arr[j++] = new Box(box[0], box[1], box[2]); // only 1 config as we are maximizing height so once sorted height is maximized in case of rotating of box
+        }
+        Arrays.sort(arr);
+        int[] dp = new int[arr.length];
+        dp[0] = arr[0].h;
+        int maxHeight = dp[0];
+        for(int i = 1; i < arr.length; i++) {
+            int max = 0;
+            for(int k = i - 1; k >= 0; k--) {
+                if(arr[k].l >= arr[i].l && arr[k].b >= arr[i].b && arr[k].h >= arr[i].h) {
+                    max = Math.max(max, dp[k]);
+                }
+            }
+            dp[i] = max + arr[i].h;
+            maxHeight = Math.max(maxHeight, dp[i]);
+        }
+        return maxHeight;
+    }
+
+    // https://leetcode.com/problems/palindromic-substrings/
+    // 647. Palindromic Substrings
+    // (Very important)
+    // O(n2)
+    public int countSubstrings(String s) {
+        int n = s.length();
+        int count = 0;
+        boolean[][] dp = new boolean[n][n];
+        for(int gap = 0;gap < n;gap++){
+            for(int i=0,j=gap;j<n;i++,j++){
+                if(gap == 0){
+                    dp[i][j] =  true;
+                } else if(gap == 1){
+                    if(s.charAt(i) == s.charAt(j)){
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = false;
+                    }
+                } else {
+                    dp[i][j] = ((s.charAt(i) == s.charAt(j)) && dp[i+1][j-1] );
+                }
+                if (dp[i][j]) count += 1;
+            }
+        }
+        return count;
+    }
+
+    // https://leetcode.com/problems/longest-palindromic-substring/
+    // 5. Longest Palindromic Substring
+    public String longestPalindrome(String s) {
+        int n = s.length();
+        int x = 0;
+        int y = 0;
+        boolean[][] dp = new boolean[n][n];
+        for(int gap = 0;gap < n;gap++){
+            for(int i=0,j=gap;j<n;i++,j++){
+                if(gap == 0){
+                    dp[i][j] =  true;
+                } else if(gap == 1){
+                    if(s.charAt(i) == s.charAt(j)){
+                        dp[i][j] = true;
+                    } else {
+                        dp[i][j] = false;
+                    }
+                } else {
+                    dp[i][j] = ((s.charAt(i) == s.charAt(j)) && dp[i+1][j-1] );
+                }
+                if (dp[i][j]) {
+                    x = i;
+                    y = j;
+                }
+            }
+        }
+        return s.substring(x, y+1);
+        // return s.substring(x, y+1).length(); if length
+    }
+    // Print All Paths With Minimum Jumps
+    private static class minJumps {
+        int indx;
+        int jumps;
+        int minJumps;
+        String psf;
+        public minJumps(int indx,int jumps,int minJumps,String psf) {
+            this.indx = indx;
+            this.jumps = jumps;
+            this.minJumps = minJumps;
+            this.psf = psf;
+        }
+    }
+    // Video - 0 pending.....
+    public static void printAllPathsMinJump(int arr[]){
+        int n = arr.length;
+        int[] dp = new int[n];
+        dp[n-1] = 0;
+        for(int i=n-2;i>=0;i--){
+            int minJumps = Integer.MAX_VALUE;
+            for(int jump=1;jump<=arr[i] && i+jump<n;j++){
+                minJumps = Math.min(minJumps,dp[i + jump]);
+            }
+            dp[i] = minJumps == Integer.MAX_VALUE ? minJumps : minJumps + 1; //becuase if we add 1 in inf it goes circle
+        }
+        Queue<minJumps> qu = new LinkedList<>();
+        qu.add(new minJumps(0, arr[0], dp[0], "0"));
+        while(qu.size() > 0){
+            int sz = qu.size();
+            minJumps rem = qu.remove();
+            for(int jump=1;jump<rem.jumps && jump+rem.indx < n;jump++){
+                if(dp[rem.indx + jump] == rem.minJumps - 1){
+                    qu.add(new minJumps(rem.indx+jump, arr[rem.indx + jump], rem.minJumps - 1, rem.psf + " -> " +(rem.indx+jump)));
+                }
+            }
+
+        }
+    }
+
+    // Print All Paths With Target Sum Subset
+    // https://practice.geeksforgeeks.org/problems/fractional-knapsack-1587115620/1/
+    // Fractional Knapsack 
+    double fractionalKnapsack(int W, Item arr[], int n) {
+        
+    }
+
     public static void main(String[] args) {
         
     }
